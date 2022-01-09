2e1bb3c720a2b7af80ad655c87a7548f3e9c0582
zrama19 <ramazanovzarif@gmail.com>
2022-01-09 13:44:47 -0500

    navbar css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import './Navbar.css';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import './Navbar.css';
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
+
   return (
     <div className='header'>
       <div className='container'>
-        <h1>
+        <h1 className='cryptowatcher'>
           crypto<span className='primary'>watcher</span>
         </h1>
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
@@ -30,9 +31,9 @@ const Navbar = () => {
         </div>
         <div className='hamburger' onClick={handleClick}>
           {click ? (
-            <FaTimes size={20} sryle={{ color: '#333' }} />
+            <FaTimes size={20} style={{ color: '#333' }} />
           ) : (
-            <FaBars size={20} sryle={{ color: '#333' }} />
+            <FaBars size={20} style={{ color: '#333' }} />
           )}
         </div>
       </div>
